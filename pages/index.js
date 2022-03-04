039c065592834f30a1f6edca7d94ae0719890a09
tae100k <78027252+tae100k@users.noreply.github.com>
2022-03-04 23:50:54 +0900

    add & delete: styled-component 추가, 불필요한 파일 삭제 [#1]

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>main screen</div>;
+  return (
+    <div>
+      main page{" "}
+      <Link href="/about">
+        <a>link to about</a>
+      </Link>
+    </div>
+  );
 }
